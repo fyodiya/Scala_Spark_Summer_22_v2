e824ac3a999335360e7509c9b0ab14ffe13a98c2
Elina <88678660+fyodiya@users.noreply.github.com>
2022-07-22 11:49:43 +0300

    testing Hadoop

---
+++ src/main/scala/com/github/fyodiya/Day19TestingHadoop.scala
@@ -0,0 +1,11 @@
+package com.github.fyodiya
+
+import org.apache.spark.sql.SparkSession
+
+object Day19TestingHadoop extends App {
+
+  println(s"Testing Hadoop with Scala : ${util.Properties.versionNumberString}")
+  val spark = SparkSession.builder().appName("test").master("local").getOrCreate()
+  println(s"Session started on Spark version ${spark.version}")
+
+}
