8ec00df54f00e5459f7a7ef308109f010c875a74
Elina <88678660+fyodiya@users.noreply.github.com>
2022-07-25 19:59:51 +0300

    testing kmeans clustering

--- src/main/scala/com/github/fyodiya/Day19TestingHadoop.scala
+++ src/main/scala/com/github/fyodiya/Day19TestingHadoop.scala
@@ -4,8 +4,8 @@ import org.apache.spark.sql.SparkSession
 
 object Day19TestingHadoop extends App {
 
-  println(s"Testing Hadoop with Scala : ${util.Properties.versionNumberString}")
+  println(s"Testing Hadoop with Scala: ${util.Properties.versionNumberString}")
   val spark = SparkSession.builder().appName("test").master("local").getOrCreate()
-  println(s"Session started on Spark version ${spark.version}")
+  println(s"Session started on Spark version: ${spark.version}")
 
 }
