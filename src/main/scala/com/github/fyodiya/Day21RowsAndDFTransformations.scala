4b5c0462c8e28c4e92122d1f53d8c4a9e794434f
Elina <88678660+fyodiya@users.noreply.github.com>
2022-07-26 20:58:41 +0300

    exercise with row operations or creating the DF from raw data

--- src/main/scala/com/github/fyodiya/Day21RowsAndDFTransformations.scala
+++ src/main/scala/com/github/fyodiya/Day21RowsAndDFTransformations.scala
@@ -1,7 +1,8 @@
 package com.github.fyodiya
 
 import org.apache.spark.sql.Row
-import org.apache.spark.sql.types.{LongType, StringType, StructField, StructType}
+import org.apache.spark.sql.functions.{col, column, expr}
+import org.apache.spark.sql.types.{LongType, IntegerType, DoubleType, BooleanType, StringType, StructField, StructType}
 
 object Day21RowsAndDFTransformations extends App {
 
@@ -65,6 +66,89 @@ object Day21RowsAndDFTransformations extends App {
   //necessarily recommended for production use cases.
   //val anotherDF = Seq(("Hello", 2, 1L)).toDF("col1", "col2", "col3")
 
+//let’s take a look at their most useful methods that
+  //you’re going to be using: the select method when you’re working with columns or expressions,
+  //and the selectExpr method when you’re working with expressions in strings. Naturally some
+  //transformations are not specified as methods on columns; therefore, there exists a group of
+  //functions found in the org.apache.spark.sql.functions package.
+  //With these three tools, you should be able to solve the vast majority of transformation challenges
+  //that you might encounter in DataFrames.
+
+  //select and selectExpr allow you to do the DataFrame equivalent of SQL queries on a table of data
+
+  df.select("DEST_COUNTRY_NAME").show(2)
+
+  val newDF = df.select("DEST_COUNTRY_NAME", "ORIGIN_COUNTRY_NAME")
+  newDF.show(3)
+
+  //You can select multiple columns by using the same style of query, just add more column name
+  //strings to your select method call
+
+  println("Same thing using SQL syntax")
+  //with SQL syntax it is a bit harder to debug errors / typos etc
+  val sqlWay = spark.sql("""
+      SELECT DEST_COUNTRY_NAME, ORIGIN_COUNTRY_NAME as my_ORIGIN
+      FROM dfTable
+      LIMIT 10
+      """)
+  sqlWay.show(5)
+
+  //you can refer to columns in a number of different
+  //ways; all you need to keep in mind is that you can use them interchangeably
+  //try to stick with just one type in a single project
+  df.select(
+    df.col("DEST_COUNTRY_NAME"),
+    col("DEST_COUNTRY_NAME"),
+    column("DEST_COUNTRY_NAME"),
+    //    'DEST_COUNTRY_NAME, //not used as much required implicits which are being depreceated
+    //    $"DEST_COUNTRY_NAME",
+    expr("DEST_COUNTRY_NAME")) //expression lets us do more transformations than col or column
+    .show(2)
+
+  //One common error is attempting to mix Column objects and strings. For example, the following
+  //code will result in a compiler error:
+  //df.select(col("DEST_COUNTRY_NAME"), "DEST_COUNTRY_NAME")
+
+  //expr is the most flexible reference that we can use. It can refer to a plain
+  //column or a string manipulation of a column. To illustrate, let’s change the column name, and
+  //then change it back by using the AS keyword and then the alias method on the column:
+
+  // in Scala
+  df.select(expr("DEST_COUNTRY_NAME AS destination")).show(2)
+
+  //we can use aliases instead of changing the column name
+  // in Scala
+  df.select(expr("DEST_COUNTRY_NAME as destination").alias("my destination"))
+    .show(2)
+
+
+  //create 3 Rows with the following data formats, string - holding food name, int - for holding quantity, long for holding price
+  //also boolean for holding isIt Vegan or not - so 4 data cells in each row
+  // you will need to manually create a Schema - column names thus will be food, qty, price, isVegan
+  // you  might need to import an extra type or two import org.apache.spark.sql.types.{StructField, StructType, StringType, LongType }
+  //create a dataFrame called foodFrame which will hold those Rows
+  //Use Select or/an SQL syntax to select and show only name and qty
+
+  //so 3 Rows of food each Row will have 4 entries (columns) so original data could be something like Chocolate, 3, 2.49, false
+  //in Schema , name, qty, price are required (not nullable) while isVegan could be null
+
+  val newManualSchema = new StructType(Array(
+    StructField("food_name", StringType, true),
+    StructField("quantity", IntegerType, true),
+    StructField("vegan", BooleanType , true),
+    StructField("price", DoubleType, true)))
+
+  val newRows = Seq(
+    Row("Chocolate", 2, true, 2.19),
+    Row("Tomatoes", 14, true, 2.49),
+    Row("Grapes", 358, true, 3.99)
+  )
+
+  val newRDD = spark.sparkContext.parallelize(newRows)
+  val foodFrame = spark.createDataFrame(newRDD, newManualSchema)
+
+  val foodDF = foodFrame.select("food_name", "quantity")
+  foodDF.show()
 
 
 
