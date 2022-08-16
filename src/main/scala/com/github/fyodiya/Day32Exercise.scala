a2f6e3c9e942d9966e845b87b2397d10e87c8ce0
Elina <88678660+fyodiya@users.noreply.github.com>
2022-08-16 20:56:38 +0300

    Machine Learning - exercise

---
+++ src/main/scala/com/github/fyodiya/Day32Exercise.scala
@@ -0,0 +1,40 @@
+package com.github.fyodiya
+
+import com.github.fyodiya.SparkUtilities.{getOrCreateSpark, readDataWithView}
+
+import org.apache.spark.ml.feature.RFormula
+
+  object Day32Exercise extends App {
+
+    val spark = getOrCreateSpark("Sandbox")
+
+    //load into dataframe from retail-data by-day December 1st
+    val filePath = "src/resources/retail-data/by-day/2010-12-01.csv"
+    val df = readDataWithView(spark, filePath)
+
+    //create RFormula to use Country as label and only UnitPrice and Quantity as Features
+    //make sure they are numeric columns - we do not want one hot encoding here
+    //you can leave column names at default
+
+     val formula = new RFormula()
+      .setFormula("Country ~ UnitPrice + Quantity")
+      .setFeaturesCol("MyFeatures")
+      .setLabelCol("MyLabel")
+
+    //create output dataframe with the the formula performing fit and transform
+    val outputDF = formula.fit(df).transform(df)
+    outputDF.show()
+
+    //BONUS
+    //try creating features from ALL columns in the Dec1st CSV except of course Country (using . syntax)
+    //This should generate very sparse column of features because of one hot encoding
+
+    val allColumnsFeatures = new RFormula()
+      .setFormula("Country ~ .")
+      .setFeaturesCol("MyFeatures")
+      .setLabelCol("MyLabel")
+
+    val outputAgain = allColumnsFeatures.fit(df).transform(df)
+    outputAgain.show()
+
+}
