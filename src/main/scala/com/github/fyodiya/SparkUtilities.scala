0b09ad01aaed4ebdddd960658c28952ed586e9cc
Elina <88678660+fyodiya@users.noreply.github.com>
2022-08-18 20:29:10 +0300

    spark utilities

--- src/main/scala/com/github/fyodiya/SparkUtilities.scala
+++ src/main/scala/com/github/fyodiya/SparkUtilities.scala
@@ -12,14 +12,13 @@ object SparkUtilities {
    * @param master by default it is "local", master URL to connect to
    * @return sparkSession
    */
-  def getOrCreateSpark(appName: String,
-                       partitionCount: Int = 5,
+  def getOrCreateSpark(appName:String, partitionCount:Int = 1,
                        master:String = "local",
                        verbose:Boolean = true): SparkSession = {
     if (verbose) println(s"$appName with Scala version: ${util.Properties.versionNumberString}")
-    val sparkSession = SparkSession.builder().appName(appName).master("local").getOrCreate()
+    val sparkSession = SparkSession.builder().appName(appName).master(master).getOrCreate()
     sparkSession.conf.set("spark.sql.shuffle.partitions", partitionCount)
-    if (verbose) println(s"Session started on Spark version: ${util.Properties.versionNumberString}, with ${partitionCount} partitions.")
+    if (verbose) println(s"Session started on Spark version ${sparkSession.version} with ${partitionCount} partitions")
     sparkSession
   }
 
