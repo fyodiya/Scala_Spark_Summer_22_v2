005a19f0d74ee485f9305e1cd6e6efcd10a89260
Elina <88678660+fyodiya@users.noreply.github.com>
2022-08-22 20:52:18 +0300

    Exercise - completed

---
+++ src/main/scala/com/github/fyodiya/Day34Exercise.scala
@@ -0,0 +1,44 @@
+package com.github.fyodiya
+
+import com.github.fyodiya.SparkUtilities.getOrCreateSpark
+import org.apache.spark.ml.feature.{CountVectorizer, StopWordsRemover, Tokenizer}
+
+object Day34Exercise extends App {
+
+  val spark = getOrCreateSpark("Sparky")
+
+  //use tokenized alice - from weekend exercise
+  val url = "https://www.gutenberg.org/files/11/11-0.txt"
+  val dst = "src/resources/text/Alice.txt"
+  Utilities.getTextFromWebAndSave(url, dst)
+  val df = spark.read.textFile(dst)
+    .toDF("text")
+  val tkn = new Tokenizer()
+    .setInputCol("text")
+    .setOutputCol("tokenizedWords")
+  val tokenDF = tkn.transform(df.select("text"))
+
+  //remove english stopwords
+  val englishStopWords = StopWordsRemover.loadDefaultStopWords("english")
+  val stops = new StopWordsRemover()
+    .setStopWords(englishStopWords)
+    .setInputCol("tokenizedWords")
+    .setOutputCol("withoutEnglishStopWords")
+  stops.transform(tokenDF)
+
+  //Create a CountVectorized of words/tokens/terms that occur in at least 3 documents
+  //(here that means rows)
+  val cv = new CountVectorizer()
+    .setInputCol("tokenizedWords")
+    .setOutputCol("countVec")
+    .setVocabSize(500)
+    .setMinTF(1)
+    .setMinDF(3)
+  val fittedCV = cv.fit(tokenDF)
+
+  //show first 30 rows of data
+  fittedCV.transform(tokenDF).show(30, false)
+
+  //the terms have to occur at least 1 time in each row
+
+}
